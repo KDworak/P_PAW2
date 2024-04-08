ae94d4e25531a758c40fe926c2445bca430ca981
lortdemola <97162568+lortdemola@users.noreply.github.com>
2024-04-08 16:31:59 +0200

    Update for User DAO
    Updated user DAO and add middleware as manager additionally added token and password DAOs for practical use

--- BackEnd/app/DAO/userDAO.js
+++ BackEnd/app/DAO/userDAO.js
@@ -3,6 +3,7 @@ import * as _ from 'lodash';
 import Promise from 'bluebird';
 import applicationException from '../service/applicationException';
 import mongoConverter from '../service/mongoConverter';
+import uniqueValidator from 'mongoose-unique-validator';
 
 const userRole = {
     admin: 'admin',
@@ -21,6 +22,59 @@ const userSchema = new mongoose.Schema({
     collection: 'user'
 });
 
+userSchema.plugin(uniqueValidator);
+
+const UserModel = mongoose.model('user', userSchema);
+
+function createNewOrUpdate(user) {
+    return Promise.resolve().then(() => {
+        if (!user.id) {
+            return new  UserModel(user).save().then(result => {
+                if (result) {
+                    return mongoConverter(result);
+                }
+            });
+        } else {
+            return UserModel.findByIdAndUpdate(user.id, _.omit(user, 'id'), { new: true });
+        }
+    }).catch(error => {
+        if ('ValidationError' === error.name) {
+            error = error.errors[Object.keys(error.errors)[0]];
+            throw applicationException.new(applicationException.BAD_REQUEST, error.message);
+        }
+        throw error;
+    });
+}
+
+async function getByEmailOrName(name) {
+    const result = await UserModel.findOne({ $or: [{ email: name }, { name: name }] });
+    if (result) {
+        return mongoConverter(result);
+    }
+    throw applicationException.new(applicationException.NOT_FOUND, 'User not found');
+}
+
+async function get(id) {
+    const result = await UserModel.findOne({ _id: id });
+    if (result) {
+        return mongoConverter(result);
+    }
+    throw applicationException.new(applicationException.NOT_FOUND, 'User not found');
+}
+
+async function removeById(id) {
+    return await UserModel.findByIdAndRemove(id);
+}
+
+export default {
+    createNewOrUpdate: createNewOrUpdate,
+    getByEmailOrName: getByEmailOrName,
+    get: get,
+    removeById: removeById,
+
+    userRole: userRole,
+    model: UserModel
+};
 
 
 
