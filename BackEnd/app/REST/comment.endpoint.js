71649288e69590cb6c877b3ef7ff503f34d433f1
AlbertK10 <103700277+AlbertK10@users.noreply.github.com>
2024-05-03 23:11:04 +0200

    Update comment.endpoint.js
    Added endpoints to the comment.endpoint file.

--- BackEnd/app/REST/comment.endpoint.js
+++ BackEnd/app/REST/comment.endpoint.js
@@ -4,10 +4,76 @@ import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 const storageS = multer.memoryStorage();
 const upload =  multer({ storage: storageS});
-/*
+
 const CommentEndpoint = (router) => {
+    router.put('/api/Comment/update/:id', async (req, res) => {
+        try {
+            const CommentEntry = await businessContainer.getCommentManager(req).createNewOrUpdate(req.body);
+            if (CommentEntry) {
+                res.status(201).json(CommentEntry);
+            } else {
+                res.status(500).json({ error: 'Error creating Comment entry 1' });
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Error creating Comment entry' });
+        }
+    });
 
 
-} 
-*/
+    router.put('/api/deleteComment/:id', async (req, res) => {
+        try {
+            const CommentEntry = await businessContainer.getCommentManager().remove(req.params.id);
+            if (CommentEntry) {
+                res.status(201).json(CommentEntry);
+            } else {
+                res.status(500).json({ error: 'Error creating Comment entry 1' });
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Error creating Comment entry' });
+        }       
+});
+
+    router.post('/api/createComment', async (req, res, next) => {
+        try {
+            let temp = {
+                id_IMG:req.body.id_IMG,
+                id_User:req.body.id_User ,
+                text:req.body.text ,
+                date:req.body.date,
+            };
+            const CommentEntry = await businessContainer.getCommentManager().createNewOrUpdate(temp);
+
+            if (CommentEntry) {
+                res.status(201).send('Comment added');
+            } else {
+                res.status(500).json({ error: 'Error creating Comment entry 1' });
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Error creating Comment entry' });
+        }
+    });
+
+    router.get('/api/Comment/byId/:Id', async (req, res,next) => {
+        try {
+            const Comment = await businessContainer.getCommentManager().get(req.params.id_IMG);
+            console.log(Comment);
+            res.status(200).json(Comment);
+        } catch (error) {
+            res.status(500).json({ error: 'Error fetching Comment' });
+        }
+
+    });
+    router.get('/api/Comment/by-user/:userId', async (req, res) => {
+        const userId = req.params.id_User;
+        console.log(req.params.id_User);
+        try {
+            const Comment = await businessContainer.getCommentManager(req).findByOwner(userId);
+            console.log(Comment);
+            res.status(200).json(Comment);
+        } catch (error) {
+            res.status(500).json({ error: 'Error fetching Comment' });
+        }
+
+    });
+}
 export default CommentEndpoint;
