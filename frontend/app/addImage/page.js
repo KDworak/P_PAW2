341fea4055ce68f73c110c80f6b7e34c2a2862fd
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2024-05-07 17:37:32 +0200

    Update page.js
    cosmetic corrections

--- frontend/app/addImage/page.js
+++ frontend/app/addImage/page.js
@@ -46,26 +46,26 @@ const ImageUploadForm = () => {
             <hr className='bg-myCol mb-8'/>
                 
                 <div className=''>
-                    <label htmlFor="id_user">User ID:</label><br/>
+                    <label htmlFor="id_user">ID Użytkownika:</label><br/>
                     <input type="text" id="id_user" value={id_user} onChange={(e) => setIdUser(e.target.value)} className="border border-myCol rounded " />
                 </div>
                 <div className=''>
-                    <label htmlFor="title">Title:</label><br/>
+                    <label htmlFor="title">Tytuł:</label><br/>
                     <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} className="border border-myCol rounded " />
                 </div>
                 <div className=''>
-                    <label htmlFor="description">Description:</label><br/>
+                    <label htmlFor="description">Opis:</label><br/>
                     <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} className="border border-myCol rounded " />
                 </div>
                 <div className=''>
-                    <label htmlFor="is_public">Is Public:</label>
+                    <label htmlFor="is_public">Czy Publiczne:</label>
                     <input type="checkbox" id="is_public" checked={isPublic} onChange={(e) => setIsPublic(e.target.checked)} className="ml-2 border border-myCol rounded " />
                 </div>
                 <div className=''>
                     <label htmlFor="file">Wybeirz zdjęcie:</label><br/>
                     <input type="file" id="file" accept="image/*" onChange={handleFileChange} className=" " />
                 </div><br/>
-                <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg'>Upload</button>
+                <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg'>Dodaj</button>
                 <Image src='/addImageLogo.png' width='200' height='200' alt='addImage logo' className='mt-[-300px] ml-[50%] hidden lg:block'/>
                 {message && <p className='text-red lg:mt-[102px]'>{message}</p>}
                 
