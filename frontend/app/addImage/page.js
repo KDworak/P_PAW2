c01e241e72ae11f2455f47ab3dd388d2dd7d0c40
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2024-05-10 11:18:09 +0200

    Update page.js
    Updated form styles

--- frontend/app/addImage/page.js
+++ frontend/app/addImage/page.js
@@ -41,31 +41,31 @@ const ImageUploadForm = () => {
     return (
         <div>
             
-            <form onSubmit={handleSubmit} className='w-[50%] mx-auto'>
+            <form onSubmit={handleSubmit} className='w-[90%] lg:w-[50%] mx-auto'>
             <p className='my-8 text-[24px] text-myCol text-red text-center lg:text-left'>Dodaj zdjęcie</p>
-            <hr className='bg-myCol mb-8'/>
+            <hr className='bg-myCol mb-8 h-0.5'/>
                 
                 <div className=''>
-                    <label htmlFor="id_user">ID Użytkownika:</label><br/>
-                    <input type="text" id="id_user" value={id_user} onChange={(e) => setIdUser(e.target.value)} className="border border-myCol rounded " />
+                    <label htmlFor="id_user">User ID:</label><br/>
+                    <input type="text" id="id_user" value={id_user} onChange={(e) => setIdUser(e.target.value)} className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol " />
                 </div>
                 <div className=''>
-                    <label htmlFor="title">Tytuł:</label><br/>
-                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} className="border border-myCol rounded " />
+                    <label htmlFor="title">Title:</label><br/>
+                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" />
                 </div>
                 <div className=''>
-                    <label htmlFor="description">Opis:</label><br/>
-                    <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} className="border border-myCol rounded " />
+                    <label htmlFor="description">Description:</label><br/>
+                    <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" />
                 </div>
                 <div className=''>
-                    <label htmlFor="is_public">Czy Publiczne:</label>
+                    <label htmlFor="is_public">Is Public:</label>
                     <input type="checkbox" id="is_public" checked={isPublic} onChange={(e) => setIsPublic(e.target.checked)} className="ml-2 border border-myCol rounded " />
                 </div>
                 <div className=''>
                     <label htmlFor="file">Wybeirz zdjęcie:</label><br/>
                     <input type="file" id="file" accept="image/*" onChange={handleFileChange} className=" " />
                 </div><br/>
-                <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg'>Dodaj</button>
+                <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg'>Upload</button>
                 <Image src='/addImageLogo.png' width='200' height='200' alt='addImage logo' className='mt-[-300px] ml-[50%] hidden lg:block'/>
                 {message && <p className='text-red lg:mt-[102px]'>{message}</p>}
                 
