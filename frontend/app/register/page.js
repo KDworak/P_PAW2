a20494f86a19870337a415a165b6ff2c96ad1b3a
Arkadiusz Hanejko <arekimmobile@gmail.com>
2024-05-20 09:58:07 +0200

    Register page added, and improved main page

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -1,38 +1,76 @@
-"use client";
+"use client"
 import React, { useState } from 'react';
+import axios from 'axios'; 
+const Register = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordRepeat, setPasswordRepeat] = useState('');
+    const [msgFail, setMsgFail] = useState('');
+    const [msgSuccess, setMsgSuccess] = useState('');
 
-const register = () => {
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(password === passwordRepeat){
+        try {
+            const formData = new FormData();
+            formData.append('name', name);
+            formData.append('email', email);
+            formData.append('password', password);
+            console.log(formData);
+            const response = await axios.post('http://127.0.0.1:3001/api/user/create', formData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            setEmail('');
+            setName('');
+            setPassword('');
+            setPasswordRepeat('');
+            setMsgSuccess('Konto zostało utworzone.');setMsgFail('');
+            console.log(response.data); 
+       
+        } catch (error) {
+            console.error('Error:', error);
+            
+        }
+    } else {setMsgSuccess('');setMsgFail('Hasła nie są takie same !');}
+    };
 
     return (
         <>
-        <form className='w-[90%] lg:w-[50%] mx-auto'>
-            <p className='my-8 text-[24px] text-myCol text-red text-center lg:text-left'>Rejestracja</p>
-            <hr className='bg-myCol mb-8 h-0.5'/>
-            <label htmlFor="userEmail">E-mail:</label><br/>
+            <form className='w-[90%] lg:w-[50%] mx-auto' onSubmit={handleSubmit}>
+                <p className='my-8 text-[24px] text-myCol text-red text-center lg:text-left'>Rejestracja</p>
+                <hr className='bg-myCol mb-8 h-0.5'/>
+                <p className='text-[red]'>{msgFail}</p>
+                <p className='text-[green]'>{msgSuccess}</p>
+                <br/>
+                <label htmlFor="userEmail">E-mail:</label><br/>
                 <div className=' flex justify-center lg:justify-normal'>
-                    
-                    <input type="text" id="userEmail"  className=" w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" />
+                    <input type="email" id="userEmail" value={email}  onChange={(e) => setEmail(e.target.value)}  className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" required/>
                 </div>
                 <br/>
-                <label htmlFor="title">Hasło:</label><br/>
-                <div className='flex justify-center lg:justify-normal'>
-                    
-                    <input type="password" id="title" className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" />
+                <label htmlFor="userName">Nazwa:</label><br/>
+                <div className=' flex justify-center lg:justify-normal'>
+                    <input type="text" id="userName" value={name}  onChange={(e) => setName(e.target.value)}  className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" required/>
                 </div>
                 <br/>
-                <label htmlFor="titleRepeat">Powtórz hasło:</label><br/>
+                <label htmlFor="password">Hasło:</label><br/>
                 <div className='flex justify-center lg:justify-normal'>
-                    
-                    <input type="password" id="titleRepeat" className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" />
+                    <input type="password" id="password" value={password}  onChange={(e) => setPassword(e.target.value)}  className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" required/>
+                </div>
+                <br/>
+                <label htmlFor="passwordRepeat">Powtórz hasło:</label><br/>
+                <div className='flex justify-center lg:justify-normal'>
+                    <input type="password" id="passwordRepeat"  value={passwordRepeat}  onChange={(e) => setPasswordRepeat(e.target.value)}  className="w-[100%] h-[48px] text-[24px] lg:w-[400px] border border-myCol rounded bg-formInputBgCol" required />
                 </div>
                 <br/>
                 <div className='flex justify-center lg:justify-normal'>
-                <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg px-8'>Zarejestruj się</button>
+                    <button type="submit" className='bg-myCol p-2 rounded-md text-myBg shadow-lg px-8'>Zarejestruj się</button>
                 </div>
-                </form>
+            </form>
         </>
     );
 };
 
-export default register;
+export default Register;
