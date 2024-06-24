bd2c044f730b092c054e9b949da136ccc96a2e5a
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2024-06-24 15:37:51 +0200

    Update page.js

--- frontend/app/addImage/page.js
+++ frontend/app/addImage/page.js
@@ -20,37 +20,40 @@ const addImage = () => {
         setFile(e.target.files[0]);
     };
 
-     const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setIsLoading(true);
         setMessage('');
+    
         try {
-            const formData = new FormData();
-            formData.append('file', file);
-            formData.append('id_user', userId); 
-            formData.append('title', title);
-            formData.append('description', description);
-            formData.append('is_public', isPublic);
-
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/createImage`, formData, {
+            const data = {
+                file: file,  
+                id_user: userId,
+                title: title,
+                description: description,
+                is_public: isPublic
+            };
+    
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/createImage`, data, {
                 headers: {
-                    'Content-Type': 'multipart/form-data'
+                    'Content-Type': 'application/json'
                 }
             });
+    
             console.log(response);
             setMessage('Zdjęcie dodane');
-        } catch(error){  
-            console.log(error);  
-            setMessage('Błąd podczas wstawiania pliku.');          
-        }
-        finally{
+        } catch (error) {
+            console.log(error);
+            setMessage('Błąd podczas wstawiania pliku.');
+        } finally {
             setFile(null);
             setTitle('');
             setDescription('');
             setIsPublic(false);
-            setIsLoading(false); 
+            setIsLoading(false);
         }
     };
+
 
     return (
          <div>
